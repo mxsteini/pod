37cfcdc9daca880a486845c01d1e272b55b499d1
Michael Stein <michael.stein@cyperfection.de>
2019-11-08 13:05:06 +0100

    fpm is typo3-ready, write-access is not working

--- pod.sh
+++ pod.sh
@@ -12,26 +12,33 @@ command='rsync'
 source='/var/www/dev.pixelmat.ch'
 target='/var/www/www.pixelmat.ch'
 
-for i in "$@"
-do
-case $i in
-    -u|--up)
+for i in "$@"; do
+  case $i in
+  -u | --up)
     command='up'
     shift
     ;;
-    -d|--down)
+  -d | --down)
     command='down'
     shift
     ;;
-    -p|--pull)
+  -p | --pull)
     command='pull'
     shift
     ;;
-    -c|--create)
+  -c | --create)
     command='create'
     shift
     ;;
-    -r|--rm)
+  --composer)
+    command='composer'
+    shift
+    ;;
+  --build)
+    command='build'
+    shift
+    ;;
+  -r | --rm)
     command='rm'
     shift
     ;;
@@ -41,55 +48,68 @@ done
 #      --volume /home/mst/Projekte/Docker/wwwlogs:/var/log/httpd24:Z \
 #      --volume /home/mst/Projekte/Docker/apache2/:/usr/local/apache2/conf/ \
 case "${command}" in
-  create2)
-    podman pod create --infra --name cyzpod -p 8080:8080
-    podman run -dit \
-      -u 0 \
-      --pod cyzpod \
-      --name httpd \
-      -e HTTPD_LOG_TO_VOLUME=1 \
-      --volume /home/mst/Projekte/Docker/wwwlogs:/var/log/httpd24:Z \
-      --volume /home/mst/Projekte/Docker/:/var/www:Z \
-      --volume /home/mst/Projekte/Docker/bain/:/var/www/bain:Z \
-      rhscl/httpd-24-rhel7
-    ;;
-  create)
-    podman pod create --infra --name cyzpod -p 8080:80 -p 3306
-    podman run -dit \
-      --pod cyzpod \
-      --name httpd \
-      --volume /home/mst/Projekte/BAIN3/typo3/:/var/www/:z \
-      --volume /home/mst/Projekte/Docker/logs/:/usr/local/apache2/logs/:Z \
-      --volume /home/mst/Projekte/Docker/etc/apache2/:/usr/local/apache2/conf/:Z \
-      httpd:2.4
-    podman run -dit \
-      --pod cyzpod \
-      --name php72 \
-      --volume /home/mst/Projekte/BAIN3/typo3/:/var/www/html/:Z \
-      php:7.2-fpm
-    podman run -dit \
-      --pod cyzpod \
-      --name db \
-      --volume /home/mst/Projekte/Docker/db/:/var/lib/mysql:Z \
-      -e MYSQL_ROOT_PASSWORD=root \
-      mariadb:latest
-#      cat /usr/local/apache2/conf/original/httpd.conf
-#      cat /usr/local/apache2/conf/extra/proxy-html.conf
-#      cat /var/www/index.html
-    ;;
-  up)
-    podman pod start cyzpod
-    ;;
-  down)
-    podman pod stop cyzpod
-    ;;
-  rm)
-    ./pod.sh --down
-    podman rm httpd
-    podman rm php72
-    podman rm db
-    podman pod rm cyzpod
-    ;;
+build)
+  podman build --tag myfpm:7.2 -f Dockerfiles/php7.2
+  #  podman build --tag mycomposer -f Dockerfiles/composer
+  ;;
+create)
+  podman pod create --infra --name cyzpod -p 8080:80 -p 3306
+  podman run -dit \
+    --pod cyzpod \
+    --name httpd \
+    --volume /home/mst/Projekte/BAIN3/typo3/:/var/www/:z \
+    --volume /home/mst/Projekte/Docker/log/:/usr/local/apache2/logs/:Z \
+    --volume /home/mst/Projekte/Docker/etc/apache2/:/usr/local/apache2/conf/:Z \
+    httpd:2.4
+  podman run -dit \
+    --pod cyzpod \
+    --name php72 \
+    --privileged \
+    --user 1000:1000 \
+    --security-opt label=disable \
+    --volume /home/mst/Projekte/Docker/log/:/var/log/:Z \
+    --volume /home/mst/Projekte/BAIN3/typo3/:/var/www/html/:z \
+    --volume ./etc/php7.2/:/usr/local/etc/php-fpm.d/:z \
+    myfpm:7.2
+  podman run -dit \
+    --pod cyzpod \
+    --name db \
+    --volume /home/mst/Projekte/Docker/db/:/var/lib/mysql:Z \
+    -e MYSQL_ROOT_PASSWORD=root \
+    mariadb:latest
+  #      cat /usr/local/apache2/conf/original/httpd.conf
+  #      cat /usr/local/apache2/conf/extra/proxy-html.conf
+  #      cat /var/www/index.html
+  ;;
+up)
+  podman pod start cyzpod
+  ;;
+composer)
+  podman run --name mycomposer --rm --interactive --tty \
+    --privileged \
+    --user 1000:1000 \
+    --security-opt label=disable \
+    --volume /home/mst/Projekte/BAIN3/typo3/:/app/:Z \
+    composer install --ignore-platform-reqs --no-scripts --prefer-source
+  ;;
+down)
+  podman pod stop cyzpod
+  ;;
+rm)
+  ./pod.sh --down
+  podman rm httpd
+  podman rm php72
+  podman rm db
+  podman pod rm cyzpod
+  ;;
 esac
+
+exit
+
 #      --volume /home/mst/Projekte/Docker/html/:/usr/local/apache2/htdocs/:z \
 #      --volume /home/mst/Projekte/Docker/apache2/:/usr/local/apache2/conf/:Z \
+
+--privileged \
+  --userns=""
+--user 1000:1000 \
+  --security-opt label=disable
